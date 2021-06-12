c23185a4a0f0c26039fd20edf5c044ec463585ab
THiragi <w1th1nmybr41n@gmail.com>
2021-06-12 16:48:12 +0900

    add: callback to server.listen

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@ server.get('/', async (req: FastifyRequest, res: FastifyReply) => {
   return { hello: 'world' };
 });
 
-server.listen(3000);
+server.listen(3000, (err: Error, add: string) => {
+  if (err) throw err;
+  server.log.info(`server listening on ${add}`);
+});
