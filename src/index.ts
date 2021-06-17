cf366724cb4e4751802ca2425aa965f7f465eb26
THiragi <w1th1nmybr41n@gmail.com>
2021-06-17 10:48:42 +0900

    add interfaces and prevalidation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import { fastify } from 'fastify';
+import { request } from 'http';
+
+interface IQuerystring {
+  username: string;
+  password: string;
+}
+
+interface IHeaders {
+  'h-Custom': string;
+}
 
 const server = fastify({ logger: true });
 
-server.get('/', (_, reply) => {
-  reply.send({ hello: 'world' });
-});
+server.get<{ Querystring: IQuerystring; Headers: IHeaders }>(
+  '/auth',
+  {
+    preValidation: (request, reply, done) => {
+      const { username, password } = request.query;
+      done(username !== 'admin' ? new Error('Must be admin') : undefined);
+    },
+  },
+  async (request, reply) => {
+    const customHeader = request.headers['h-Custom'];
+
+    return `logged in!`;
+  }
+);
 
 server.listen(3000, (err, address) => {
   if (err) {
