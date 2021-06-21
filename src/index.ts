73bd3d8baf5f491ce2be0146b6bdf3d59d8fadad
THiragi <w1th1nmybr41n@gmail.com>
2021-06-21 10:22:43 +0900

    clean up index

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,11 @@
-import { fastify, FastifyInstance, RouteShorthandOptions } from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from 'http';
-import { PrismaClient } from '@prisma/client';
-import { resolve } from 'path/posix';
-import { Static, Type } from '@sinclair/typebox';
-// import BlogRoutes from './routes/blogs';
+import { fastify } from 'fastify';
+import { router } from './routes';
 
-const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
+const app = fastify({
   logger: true,
 });
 
-const prisma = new PrismaClient();
-
-const BodyScheme = Type.Object({
-  title: Type.String(),
-  content: Type.String(),
-  authorEmail: Type.String(),
-});
-
-type BodyType = Static<typeof BodyScheme>;
-
-const ResponseSchema = Type.Object({
-  id: Type.Number(),
-  title: Type.String(),
-});
-
-const options: RouteShorthandOptions = {
-  schema: {
-    body: BodyScheme,
-    response: {
-      200: ResponseSchema,
-    },
-  },
-};
-
-app.get('/', (_, reply) => {
-  reply.send({ hello: 'World' });
-});
-
-app.get('/feed', async (_, reply) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true },
-  });
-  reply.send(posts);
-});
-
-app.post<{ Body: BodyType }>(`/post`, options, async (request, reply) => {
-  const { title, content, authorEmail } = request.body;
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
-    },
-  });
-  reply.send(result);
-});
-
-// BlogRoutes.forEach((route) => {
-//   app.route(route);
-// });
+app.register(router);
 
 app.listen(3000, (err, address) => {
   if (err) {
